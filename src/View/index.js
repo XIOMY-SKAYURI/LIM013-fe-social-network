eb8323cfe4083ac07170a146b71b2db04f6e5288
katylurdht <katyhuamanitito1@gmail.com>
2020-11-12 09:12:50 -0500

    creando vistas

---
+++ src/View/index.js
@@ -0,0 +1,9 @@
+import home from './home.js';
+import SignIn from './SignIn.js';
+import SignUp from './SignUp'; // importamos
+
+export const components = {
+  hom: home,
+  SignI: SignIn,
+  SignU: SignUp,
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-// Este es el punto de entrada de tu aplicacion
+import { changeTmp } from './view-controller/index.js'; // importamos funcion de controlador
 
-import { myFunction } from './lib/index.js';
+//  changeTmp();
 
-myFunction();
+// cada vez que escuches cargar ejecuta funcion init
+const init = () => {
+  changeTmp(window.location.hash);
+  window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+};
+
+window.addEventListener('load', init);
