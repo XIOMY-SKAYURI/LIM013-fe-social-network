ac8de7648baa752398e6c3d266a7f7d94011144a
XIOMY-SKAYURI <xiomygarciaguerrero@gmail.com>
2020-11-17 15:24:54 -0500

    anuevos

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import { changeTmp } from './view-controller/index.js'; // importamos funcion de
 
 // cada vez que escuches cargar ejecuta funcion init
 const init = () => {
-  changeTmp(window.location.hash);
-  window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+    changeTmp(window.location.hash);
+    window.addEventListener('hashchange', () => changeTmp(window.location.hash));
 };
 
 window.addEventListener('load', init);
--- src/view-controller/index.js
+++ src/view-controller/index.js
@@ -1,16 +1,19 @@
 import { components } from '../View/index.js';
 
 export const changeTmp = (hash) => {
-  // console.log(hash);
-  // const id = hash.split('/')[1];
-  const container = document.getElementById('container');
-  container.innerHTML = '';
+    // console.log(hash);
+    // const id = hash.split('/')[1];
+    const container = document.getElementById('container');
+    container.innerHTML = '';
 
-  switch (hash) {
-    case '#/SignIn': { return container.appendChild(components.SignI()); }
-    case '#/SignUp': { return container.appendChild(components.SignU()); }
-    case '#/Home': { return container.appendChild(components.hom()); }
-    default:
-      return container.appendChild(components.different());
-  }
+    switch (hash) {
+        case '#/SignIn':
+            { return container.appendChild(components.SignI()); }
+        case '#/SignUp':
+            { return container.appendChild(components.SignU()); }
+        case '#/Home':
+            { return container.appendChild(components.hom()); }
+        default:
+            return container.appendChild(components.different());
+    }
 };
